fd2cda8eb257d5de609b79a56364019c002916c8
Mike Shu <heliax44@gmail.com>
2022-10-05 18:54:56 +0400

    update: set "CreateFaq" page as deprecated

--- app/Filament/Resources/FaqResource/Pages/CreateFaq.php
+++ app/Filament/Resources/FaqResource/Pages/CreateFaq.php
@@ -6,6 +6,10 @@ use App\Filament\Resources\FaqResource;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
+/**
+ * @package App\Filament\Resources\FaqResource\Pages
+ * @deprecated
+ */
 class CreateFaq extends CreateRecord
 {
     protected static string $resource = FaqResource::class;
