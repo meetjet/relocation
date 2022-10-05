d6949eb017a8ebcc0f3982a6bcdaf7cebe0f92e1
Mike Shu <heliax44@gmail.com>
2022-10-05 17:10:17 +0400

    add: "create_faqs_table" migration

---
+++ database/migrations/2022_10_04_103127_create_faqs_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Enums\FaqStatus;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('faqs', static function (Blueprint $table) {
+            $table->id();
+            $table->uuid()->nullable()->unique();
+
+            $table->text('original');
+            $table->text('question')->nullable();
+            $table->text('answer')->nullable();
+            $table->string('status')->default(FaqStatus::CREATED);
+            $table->boolean('visibility')->default(true);
+            $table->jsonb('data')->nullable();
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('faqs');
+    }
+};
