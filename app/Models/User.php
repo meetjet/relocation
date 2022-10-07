213793769795972e65ce95e0ea7a0a21c20d1b8e
Mike Shu <heliax44@gmail.com>
2022-10-07 10:13:44 +0400

    update: added soft delete to "User" model

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
@@ -15,6 +16,7 @@ class User extends Authenticatable
 {
     use HasApiTokens;
     use HasFactory;
+    use SoftDeletes;
     use HasProfilePhoto;
     use HasTeams;
     use Notifiable;
