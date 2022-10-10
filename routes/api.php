0210c6ba7e52bbe689b2299b94ea69183f67dc4d
Mike Shu <heliax44@gmail.com>
2022-10-10 12:46:46 +0400

    Revert "add: LogExchange middleware"
    This reverts commit 3fea51eb

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\Api\TelegramController;
-use App\Http\Middleware\LogExchange;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,7 +19,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::prefix('bots')->middleware(LogExchange::class)->group(function () {
+Route::prefix('bots')->group(function () {
     // Telegram webhook
     Route::post('/telegram', TelegramController::class);
 
