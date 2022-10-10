45cea67ab9e267d99454161891ab0790999d9c3c
Mike Shu <heliax44@gmail.com>
2022-10-10 12:47:08 +0400

    Revert "add: logging telegram webhook"
    This reverts commit b72d434c

--- app/Telegram/Commands/StartCommand.php
+++ app/Telegram/Commands/StartCommand.php
@@ -12,7 +12,6 @@ class StartCommand
      */
     public function __invoke(Nutgram $bot): void
     {
-        logger('Invoke "start" command');
         $bot->sendMessage($this->getWelcomeMessage(), [
             'parse_mode' => ParseMode::HTML,
             'disable_web_page_preview' => true,
