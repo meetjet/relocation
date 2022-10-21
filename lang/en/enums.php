aaf1a046266aa0fe62a17101f20f0335ba024a59
Mike Shu <heliax44@gmail.com>
2022-10-21 18:35:50 +0400

    update: translations

--- lang/en/enums.php
+++ lang/en/enums.php
@@ -2,12 +2,23 @@
 
 use App\Enums\Countries;
 use App\Enums\FaqStatus;
+use App\Enums\ListingCategoryStatus;
+use App\Enums\ListingItemStatus;
 
 return [
     FaqStatus::class => [
         FaqStatus::CREATED => 'Created',
         FaqStatus::PUBLISHED => 'Published',
         FaqStatus::REJECTED => 'Rejected',
+    ],
+    ListingCategoryStatus::class => [
+        ListingCategoryStatus::ACTIVE => 'Active',
+        ListingCategoryStatus::INACTIVE => 'Inactive',
+    ],
+    ListingItemStatus::class => [
+        ListingItemStatus::CREATED => 'Created',
+        ListingItemStatus::PUBLISHED => 'Published',
+        ListingItemStatus::REJECTED => 'Rejected',
     ],
     Countries::class => [
         Countries::ARM => 'Armenia',
