6f5131bdd519a1ab68f19238a3b078f7e9ef6047
Mike Shu <heliax44@gmail.com>
2022-10-21 18:40:32 +0400

    add: "create_listing_categories_table" migration

---
+++ database/migrations/2022_10_19_055847_create_listing_categories_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Enums\ListingCategoryStatus;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('listing_categories', static function (Blueprint $table) {
+            $table->id();
+            $table->uuid()->nullable()->unique();
+            $table->string('slug')->nullable()->unique();
+
+            $table->string('title')->nullable();
+            $table->text('description')->nullable();
+            $table->string('status')->default(ListingCategoryStatus::ACTIVE);
+            $table->boolean('visibility')->default(false);
+            $table->nestedSet();
+            $table->jsonb('data')->nullable();
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('listing_categories');
+    }
+};
