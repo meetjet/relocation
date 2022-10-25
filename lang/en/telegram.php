cbfc0e88d09ab730a0682f7d69e38a9601d62254
Mike Shu <heliax44@gmail.com>
2022-10-25 16:59:06 +0400

    update: translations

--- lang/en/telegram.php
+++ lang/en/telegram.php
@@ -1,7 +1,9 @@
 <?php
 
 return [
-    'bot' => [
+    'fallback' => "Sorry, I don't understand the command.",
+    'exception' => "<b>Whoops!\nSomething went wrong!</b>",
+    'default' => [
         'start' => [
             'description' => "Restart bot.",
             'welcome' => "Relocation Digital Bot welcomes you.",
@@ -14,7 +16,19 @@ return [
             'end' => "Thanks for your question! You will be notified as soon as a response is received. To submit another question, type the /:command command. To see other people's questions, follow the link - :link",
             'reply' => "You got the answer to the question asked earlier! To view it, follow the link: :link",
         ],
-        'fallback' => "Sorry, I don't understand the command.",
-        'exception' => "<b>Whoops!\nSomething went wrong!</b>",
+    ],
+    'armenian' => [
+        'start' => [
+            'description' => "Restart bot.",
+            'welcome' => "Relocation Digital Bot welcomes you.",
+            'manual' => "To ask a question, type the \:command command or click on the corresponding menu button."
+        ],
+        'question' => [
+            'description' => "Ask a question.",
+            'start' => "Please write your question.",
+            'unsupported' => "This message format is not yet supported.",
+            'end' => "Thanks for your question! You will be notified as soon as a response is received. To submit another question, type the /:command command. To see other people's questions, follow the link - :link",
+            'reply' => "You got the answer to the question asked earlier! To view it, follow the link: :link",
+        ],
     ],
 ];
