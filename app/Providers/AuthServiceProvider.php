aa94b8fc25688fa6d9092d5945b7e488205f4f17
Mike Shu <heliax44@gmail.com>
2022-10-28 09:53:03 +0400

    add: authorization for log viewer

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,9 +4,12 @@ namespace App\Providers;
 
 use App\Models\ConnectedAccount;
 use App\Models\Team;
+use App\Models\User;
 use App\Policies\ConnectedAccountPolicy;
 use App\Policies\TeamPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+use Opcodes\LogViewer\LogFile;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -29,6 +32,19 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        // Return true if the user is allowed access to the Log Viewer.
+        Gate::define('viewLogViewer', static function (?User $user) {
+            return $user && in_array($user->email, config('log-viewer.access_allowed_email'), true);
+        });
+
+        // Return true if the user is allowed to download the specific log file.
+        Gate::define('downloadLogFile', static function (?User $user, LogFile $file) {
+            return $user && in_array($user->email, config('log-viewer.download_allowed_email'), true);
+        });
+
+        // Return true if the user is allowed to delete the specific log file.
+        Gate::define('deleteLogFile', static function (?User $user, LogFile $file) {
+            return $user && in_array($user->email, config('log-viewer.delete_allowed_email'), true);
+        });
     }
 }
