b192c8dd5658b9b6e34e45a1af4427b3e40b0d9f
Mike Shu <heliax44@gmail.com>
2022-10-31 15:50:05 +0400

    install: "mad-web/laravel-initializer" package

---
+++ app/Install.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use MadWeb\Initializer\Contracts\Runner;
+
+class Install
+{
+    public function production(Runner $run): void
+    {
+        //
+    }
+
+    public function local(Runner $run): void
+    {
+        //
+    }
+}
---
+++ app/Update.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use MadWeb\Initializer\Contracts\Runner;
+
+class Update
+{
+    public function production(Runner $run): void
+    {
+        //
+    }
+
+    public function local(Runner $run): void
+    {
+        //
+    }
+}
