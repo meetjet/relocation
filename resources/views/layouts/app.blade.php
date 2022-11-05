773e23dcc5a9c58733ca6746bdfb1cce81ca2960
Stelios Baglaridis <stelios@baglaridis.com>
2022-11-05 15:42:27 +0200

    wip

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,10 +19,15 @@
         <!-- Styles -->
         @livewireStyles
     </head>
-    <body class="font-sans antialiased">
+    <body
+        class="font-sans antialiased"
+        :class="{ 'overflow-y-hidden': openside }"
+        x-data="{ openside: false }"
+        @resize.window="openside = false"
+    >
         <x-jet-banner />
 
-        <div class="min-h-screen">
+        <div class="min-h-screen lg:ml-[320px] transition-all duration-300">
             @livewire('navigation-menu')
 
             <div class="container mx-auto px-4 sm:px-6 lg:px-8 flex">
@@ -42,6 +47,18 @@
                 </div>
             </div>
         </div>
+
+        <div
+            class="fixed top-0 left-0 w-screen bg-black bg-opacity-20" :class="{ 'h-screen': openside }"
+            @click="openside = false"
+        ></div>
+
+        <aside
+            class="w-[320px] bg-red-500 min-h-screen fixed top-0 -translate-x-full lg:translate-x-0 transition-transform duration-300"
+            :class="{'translate-x-0': openside }"
+        >
+
+        </aside>
 
         @stack('modals')
 
