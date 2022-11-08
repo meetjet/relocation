945e252f41191c5addd45d4b10c41e6c46bc3d81
Zorca Orcinus <vs@zorca.org>
2022-11-08 03:02:06 +0300

    fix: button styles for listings

--- resources/views/livewire/load-more-listings.blade.php
+++ resources/views/livewire/load-more-listings.blade.php
@@ -32,7 +32,7 @@
                         <div class="text-[20px] leading-none font-bold">{{ $item->price }} ₽</div>
                         <a
                             href="{{ route('listings.show', ['listing' => $item->slug]) }}"
-                            class="flex justify-center bg-[#306BC9] hover:bg-opacity-90 transition-colors duration-300 text-white text-sm font-bold leading-none rounded p-3"
+                            class="flex justify-center bg-blue-500 hover:bg-blue-600 transition-colors duration-300 text-white text-sm font-bold leading-none rounded p-3"
                             tabindex="-1"
                         >{{ __('More') }}</a>
                     </div>
