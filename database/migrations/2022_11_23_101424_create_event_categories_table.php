d5ca37d8a15e156ab4ac3535dda66ce33a0096e9
Mike Shu <heliax44@gmail.com>
2022-11-25 12:07:05 +0400

    add: "create_event_categories_table" migration

---
+++ database/migrations/2022_11_23_101424_create_event_categories_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Enums\EventCategoryStatus;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('event_categories', static function (Blueprint $table) {
+            $table->id();
+            $table->uuid()->nullable()->unique();
+            $table->string('slug')->nullable()->unique();
+
+            $table->string('title')->nullable();
+            $table->text('description')->nullable();
+            $table->string('status')->default(EventCategoryStatus::ACTIVE);
+            $table->boolean('visibility')->default(false);
+            $table->nestedSet();
+            $table->jsonb('data')->nullable();
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('event_categories');
+    }
+};
