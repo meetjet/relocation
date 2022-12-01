7abdc73adfe566c8d4cb32c17b0c460d8cc3caff
Zorca Orcinus <vs@zorca.org>
2022-12-01 09:55:39 +0300

    fix: login button view

--- resources/views/components/login-button.blade.php
+++ resources/views/components/login-button.blade.php
@@ -1,4 +1,4 @@
-<div class="tg-login-button rounded-full p-2 pt-1 pl-1 md:pl-2 md:pr-3">
+<div class="rounded-full bg-[#54a9eb] text-white px-2 md:px-3 py-2">
     <i class="tg-login-button-icon"></i>
     <span class="hidden md:inline-block ml-2">{{ __("Login via Telegram") }}</span>
 </div>
