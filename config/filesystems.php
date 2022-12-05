3d9d3b1a61b7d46e78d300df7957e0c3dda72e57
Mike Shu <heliax44@gmail.com>
2022-12-05 18:46:46 +0400

    update: "filesystems" config

--- config/filesystems.php
+++ config/filesystems.php
@@ -44,6 +44,12 @@ return [
             'throw' => false,
         ],
 
+        'uploadio' => [
+            'driver' => 'local',
+            'root' => storage_path('app'),
+            'throw' => false,
+        ],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
