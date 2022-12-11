ec169992a31cd0c8e449fdbc3178f7ab63abbda8
Mike Shu <heliax44@gmail.com>
2022-12-11 10:47:41 +0400

    fix: "Edit" button an Event index page

--- resources/views/livewire/single-event.blade.php
+++ resources/views/livewire/single-event.blade.php
@@ -85,7 +85,7 @@
             </div>
         @endif
     </div>
-    @if (auth()->user()->is_admin)
+    @if (Auth::user() && Auth::user()->is_admin)
         <x-shared.edit-entity-button :url="route('filament.resources.events.edit', $entity->id)"/>
     @endif
 </article>
