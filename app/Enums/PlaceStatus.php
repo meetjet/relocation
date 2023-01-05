7a2eccc16ee91ad9353428a76aa3d80a50c50f31
Mike Shu <heliax44@gmail.com>
2023-01-05 19:25:49 +0400

    add: PlaceType enum

--- app/Enums/PlaceStatus.php
+++ app/Enums/PlaceStatus.php
@@ -5,7 +5,7 @@ namespace App\Enums;
 use BenSampo\Enum\Contracts\LocalizedEnum;
 use BenSampo\Enum\Enum;
 
-class EventPointStatus extends Enum implements LocalizedEnum
+class PlaceStatus extends Enum implements LocalizedEnum
 {
     public const ACTIVE = "active";
     public const INACTIVE = "inactive";
