79df6677ad7d13b7610219df7ada81b768ceeff9
Zorca Orcinus <vs@zorca.org>
2023-01-16 16:53:52 +0300

    add: tkt.ge link

--- resources/views/filament/widgets/georgia-info-widget.blade.php
+++ resources/views/filament/widgets/georgia-info-widget.blade.php
@@ -4,6 +4,7 @@
             <h2 class="text-2xl font-bold tracking-tight">🇬🇪 Грузия 🇬🇪</h2>
             <h3 class="text-lg sm:text-xl font-bold tracking-tight">События</h3>
             <ul class="list-disc list-inside">
+                <li><a href="https://tkt.ge/" target="_blank">Сайт продажи билетов TKT GE</a></li>
                 <li><a href="https://t.me/expats_batumi" target="_blank">Батуми 🍷🃏🎱 сХодки (6594)</a></li>
                 <li><a href="https://t.me/batumitogo" target="_blank">Куда пойдем? Батуми (871)</a></li>
                 <li><a href="https://t.me/tbiraving" target="_blank">Ночной Тбилиси (4419)</a></li>
