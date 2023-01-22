4e4b674c11db07fd1902bd13719a4b689410993c
Mike Shu <heliax44@gmail.com>
2023-01-22 15:02:26 +0400

    refactor: EventObserver

--- app/Observers/EventObserver.php
+++ app/Observers/EventObserver.php
@@ -2,24 +2,11 @@
 
 namespace App\Observers;
 
-use App\Jobs\TelegramAttachImagesJob;
 use App\Models\Event;
 
 class EventObserver
 {
     public bool $afterCommit = true;
-
-    /**
-     * Handle the Event "created" event.
-     *
-     * @param Event $event
-     */
-    public function created(Event $event): void
-    {
-        if ($event->telegram_bot_type && $event->telegram_attached_images) {
-            TelegramAttachImagesJob::dispatch($event);
-        }
-    }
 
     /**
      * Handle the Event "forceDeleted" event.
