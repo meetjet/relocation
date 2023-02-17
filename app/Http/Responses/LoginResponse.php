fe8947af03d60d39aef4a4ccd4b8cc83ab29fc47
Zorca Orcinus <vs@zorca.org>
2023-02-17 21:14:07 +0300

    Revert "fix: redirect after login"
    This reverts commit cd466c2b7367b33771a2ef943711114c600985d3.

--- app/Http/Responses/LoginResponse.php
+++ app/Http/Responses/LoginResponse.php
@@ -21,11 +21,8 @@ class LoginResponse implements LoginResponseContract
             return response()->json(['two_factor' => false]);
         }
 
-//        return $request->session()->has('return_url')
-//            ? redirect($request->session()->get('return_url'))
-//            : redirect()->intended(Fortify::redirects('login'));
-        return session()->has('socialstream.previous_url')
-            ? redirect(session()->get('socialstream.previous_url'))
+        return $request->session()->has('return_url')
+            ? redirect($request->session()->get('return_url'))
             : redirect()->intended(Fortify::redirects('login'));
     }
 }
