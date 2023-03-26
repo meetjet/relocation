1c6469a6313f9e418cdc166532961abf3fe9735c
Zorca Orcinus <vs@zorca.org>
2023-03-26 23:57:08 +0300

    fix: error with place categories

--- app/Filament/Resources/PlaceCategoryResource/Pages/ManagePlaceCategories.php
+++ app/Filament/Resources/PlaceCategoryResource/Pages/ManagePlaceCategories.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Filament\Resources\EventCategoryResource\Pages;
+namespace App\Filament\Resources\PlaceCategoryResource\Pages;
 
 use App\Filament\Resources\PlaceCategoryResource;
 use Filament\Pages\Actions;
