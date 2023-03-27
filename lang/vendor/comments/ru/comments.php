e3c8a679c0a765518f7b29c03956ef064f54aa11
Mike Shu <heliax44@gmail.com>
2023-03-27 16:54:57 +0400

    update: translations

---
+++ lang/vendor/comments/ru/comments.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'write_comment' => 'Оставить комментарий',
+    'write_reply' => 'Оставить ответ',
+    'cancel' => 'Отмена',
+    'edit' => 'Изменить',
+    'edit_comment' => 'Сохранить',
+    'no_comments_yet' => 'Комментариев пока нет…',
+    'delete' => 'Удалить',
+    'copy_link' => 'Скопировать ссылку',
+    'delete_confirmation_title' => 'Удалить комментарий',
+    'delete_confirmation_text' => 'Вы уверены? Это не может быть отменено.',
+    'create_reply' => 'Ответить',
+    'create_comment' => 'Комментировать',
+    'just_now' => 'Прямо сейчас',
+    'send_notifications' => 'Сообщите мне, когда кто-то ответит',
+    'reject_comment' => 'Отклонить',
+    'approve_comment' => 'Одобрить',
+    'awaits_approval' => 'В ожидании одобрения',
+    'guest' => 'Гость',
+    'copied' => 'Ссылка скопирована'
+];
---
+++ lang/vendor/comments/ru/notifications.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'approve_comment' => 'Одобрить комментарий',
+    'reject_comment' => 'Отклонить комментарий',
+    'view_comment' => 'Посмотреть комментарий',
+
+    'pending_comment_mail_subject' => 'Новый комментарий ожидает одобрения',
+    'pending_comment_mail_body' => 'Ожидающий комментарий :commentable_name от :commentator_name ожидает вашего одобрения',
+
+    'approved_comment_mail_subject' => 'Добавлен новый комментарий',
+    'approved_comment_mail_title' => 'Новый комментарий к ":commentable_name"',
+    'approved_comment_mail_body' => 'Сообщение от :commentator_name',
+
+    'enum_description_participating' => 'При участии',
+    'enum_description_all' => 'На все комментарии',
+    'enum_description_none' => 'Никогда',
+    'enum_longdescription_participating' => 'Получать уведомления при участии',
+    'enum_longdescription_all' => 'Получать уведомления обо всех комментариях',
+    'enum_longdescription_none' => 'Никогда не получать уведомления',
+];
